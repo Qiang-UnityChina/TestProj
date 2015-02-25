05883a3cfaf57a80caf65d43c29ed6bb2cb1bd4f
Qiang-UnityChina <qiang@unity3d.com>
2015-02-25 14:13:47 +0800

    Add Project

---
+++ EmptyProj/Assets/run.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class run : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnGUI()
+	{
+		GUILayout.Label ("This is a test to use cloud build @_@");
+	}
+}
